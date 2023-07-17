298754b8d935c32f02a2ba98bd6b766f15b06c7a
HUOJUN546987 <139749125+HUOJUN546987@users.noreply.github.com>
2023-07-17 16:47:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oGFL36PcI4kA8ffKrPI1js-1RVLk',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'Lb83lCVcAREhHj3rztJLCOf2I0dUdnWEJE4y7AnTMos',
+      useTemplateId: 'RacCCzr8t3dc5VhDHx1Mumn1vuY67pIdMcQE7KCbLuM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-19',
       festivals: [
