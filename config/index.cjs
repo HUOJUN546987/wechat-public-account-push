1ffdff8838df9de7d7ff3fb26c9d2be2b9f1b326
HUOJUN546987 <139749125+HUOJUN546987@users.noreply.github.com>
2023-07-17 20:27:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,25 +21,25 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '哼唧唧',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: ['oGFL36PcI4kA8ffKrPI1js-1RVLk', 'oGFL36Hmr_FuJrPURd_oRdaKj670']
+      id: 'oGFL36Hmr_FuJrPURd_oRdaKj670','oGFL36PcI4kA8ffKrPI1js-1RVLk'
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'RacCCzr8t3dc5VhDHx1Mumn1vuY67pIdMcQE7KCbLuM',
+      useTemplateId: 'h7ubvVuw_XNluiAGyT3vYIQVXcU7JqrTkppY31iK9o0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-19',
       festivals: [
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '哼唧唧', year: '2004', date: '01-19',
+          type: '生日', name: '李四', year: '1996', date: '09-31',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2023', date: '02-23',
+          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-02-23' },
-        // 在一起的日子
+        // 结婚纪念日
         { keyword: 'marry_day', date: '2023-07-09' },
       ],
     },
@@ -48,4 +48,3 @@ const USER_CONFIG = {
 }
 
 module.exports = USER_CONFIG
-
